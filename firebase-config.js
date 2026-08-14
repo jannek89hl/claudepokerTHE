f8f7a47f04f77ceb90097cf21a118c213f24caa9
jannek89 <golden.treasure59@gmail.com>
2026-08-14 17:12:24 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+//
+// Paste the config object from your Firebase project here.
+// Firebase console > Project settings > General > Your apps > SDK setup and configuration.
+// This file is meant to be public. Firebase web config values are not secret;
+// access is controlled by the Firestore rules you set in the console, not by hiding this file.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-i2qPbyQT6cN_k2N5t-uyrKe9JgNuaBM",
+  authDomain: "poland-poker.firebaseapp.com",
+  projectId: "poland-poker",
+  storageBucket: "poland-poker.firebasestorage.app",
+  messagingSenderId: "503024501142",
+  appId: "1:503024501142:web:694dfdefa7c77488709db7",
+};
